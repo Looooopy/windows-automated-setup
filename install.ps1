fa0060ac8af942700d4ee98a6400da206d74f9ad
Looooopy <giorgio.salmi@stena.com>
2017-03-13 00:43:32 +0100

    Add chocolatey install/upgrade

--- install.ps1
+++ install.ps1
@@ -1,1 +1,22 @@
+Try
+{
+  choco --version
+  $chocolateyInstalled=$True
+}
+catch
+{
+  $chocolateyInstalled=$False
+}
+
+if($chocolateyInstalled)
+{
+  Write-Host "Update Chocolatey"
+  choco upgrade chocolatey
+}
+else
+{
+  Write-Host "Install Chocolatey"
+  iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+}
+
 ./windows-reg/install-reg.ps1
