64a293d5bebafb9de2a093f53af1d32d60b34e4a
Looooopy <giorgio.salmi@stena.com>
2017-03-13 00:05:37 +0100

    Add script that check if correct windows version is used and execute scripts on that.

---
+++ windows-reg/install-reg.ps1
@@ -0,0 +1,12 @@
+#Requires -RunAsAdministrator
+
+Write-Host "Adding Registry:" -foregroundcolor "DarkCyan"
+$major = [environment]::OSVersion.Version.Major
+if( $major -eq 10)
+{
+  .\windows-reg\windows10\add-long-paths.ps1
+}
+else
+{
+  echo [environment]::OSVersion.Version.Major
+}
