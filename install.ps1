ac80c7b48d36192cfb39d42500ec753187c35518
Looooopy <giorgio.salmi@stena.com>
2017-04-15 06:05:43 +0200

    download the released version of AtomProxyCmd and dependencies from github

--- install.ps1
+++ install.ps1
@@ -25,5 +25,10 @@ Import-Module Boxstarter.Chocolatey
 ./windows-reg/install-reg.ps1
 
 # Copy atom proxy that is used from bash on ubuntu on windows to parse filepaths
+New-Item ./tools/AtomProxyCmd/bin/1.0.0.1/ -ItemType Directory
+$release_version="0.0.1"
+$atomProxyCmdVersion="1.0.0.1"
+wget "https://github.com/Looooopy/windows-automated-setup/releases/download/$release_version/AtomProxyCmd.exe" -OutFile "./tools/AtomProxyCmd/bin/$atomProxyCmdVersion/AtomProxyCmd.exe"
+wget "https://github.com/Looooopy/windows-automated-setup/releases/download/$release_version/CommandLine.dll" -OutFile "./tools/AtomProxyCmd/bin/$atomProxyCmdVersion//CommandLine.dll"
 $atompath = Split-Path (Get-Command atom).Path
-cp ./tools/AtomProxyCmd/bin/1.0.0.1/* $atompath
+cp ./tools/AtomProxyCmd/bin/$atomProxyCmdVersion/* $atompath
