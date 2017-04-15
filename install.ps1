0693fc1210c3102838a5a325b080790c8f1fba5a
Looooopy <giorgio.salmi@stena.com>
2017-04-15 06:24:45 +0200

    Add Windows config, Dev things/folders/config

--- install.ps1
+++ install.ps1
@@ -19,10 +19,30 @@ else
   iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 }
 
+# Install BoxStarter
 CINST Boxstarter
 Import-Module Boxstarter.Chocolatey
 
+
+# Windows config
 ./windows-reg/install-reg.ps1
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar -EnableOpenFileExplorerToQuickAccess -EnableShowRecentFilesInQuickAccess -EnableShowFrequentFoldersInQuickAccess -EnableExpandToOpenFolder
+Enable-RemoteDesktop
+Disable-InternetExplorerESC
+Disable-MicrosoftUpdate
+Disable-BingSearch
+
+# Dev things
+CINST -y git.install
+CINST -y cmder
+CINST -y Atom
+CINST -y wireshark
+
+# Dev folders
+New-Item -path "c:\projects\git" -type directory -force
+New-Item -path "c:\projects\visualstudioonline" -type directory -force
+
+#Dev config
 
 # Copy atom proxy that is used from bash on ubuntu on windows to parse filepaths
 New-Item ./tools/AtomProxyCmd/bin/1.0.0.1/ -ItemType Directory
