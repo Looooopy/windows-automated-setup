de981ebd7ff7bd9e5269d455f7fb3523757a058a
Looooopy <giorgio.salmi@stena.com>
2017-04-15 03:56:11 +0200

    Added tool source for AromProxyCmd that are used to proxy and parse file paths from bash on ubuntu on windows to be able to use the standard windows file paths. e.g.) Path on ubuntu "/mnn/c/xx" will become "c:\xx" i windows

---
+++ tools/AtomProxyCmd/AtomProxyCmd/Options.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace AtomProxyCmd
+{
+    class Options
+    {
+        [Option('p', "password", Required = true, HelpText = "current user password that should run the proxy.")]
+        public string Password { get; set; }
+
+        [Option('u', "username", Required = true, HelpText = "current user name that should run the proxy.")]
+        public string UserName { get; set; }
+
+        //[Option('f', "filepaths", HelpText = "current files or paths that should be opened in atom.")]
+        [OptionList('f', "filepaths", Separator = ';', HelpText = "current files or paths that should be opened in atom, they should be separated with space.")]
+        public IList<string> FilePaths { get; set; } 
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ tools/AtomProxyCmd/AtomProxyCmd/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace AtomProxyCmd
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Net;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                if (options.UserName == null)
+                    throw new ArgumentNullException(nameof(options.UserName));
+                if (options.Password == null)
+                    throw new ArgumentNullException(nameof(options.Password));
+
+                var list = new List<string>();
+                foreach (var fileName in options.FilePaths)
+                {
+                    var reg = new Regex(@"^/mnt/(\w+)/(.*)", RegexOptions.IgnoreCase);
+                    var matches = reg.Matches(fileName);
+                    if (matches.Count == 1)
+                    {
+                        var groups = matches[0].Groups;
+                        var text = groups[1].Value + ":\\" + groups[2].Value;
+                        list.Add(text);
+                    }
+                    else
+                    {
+                        list.Add(fileName);
+                    }
+                }
+
+                ProcessStartInfo startInfo = new ProcessStartInfo("atom.cmd");
+                startInfo.Arguments = list.Count == 0 ? "" : list.Aggregate((x, y) => x + " " + y).Replace("/", "\\");
+                startInfo.UseShellExecute = false;
+                startInfo.UserName = options.UserName;
+                startInfo.Password = new NetworkCredential("", options.Password).SecurePassword;
+                Process.Start(startInfo);
+            }
+        }
+    }
+}
